306fe356fdc591e666320da790026dd9caf864b7
Rico Kaltofen <r.kaltofen@heimrich-hannot.de>
2018-04-18 15:37:17 +0200

    1.0.2, check changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,11 @@
 # Changelog
 All notable changes to this project will be documented in this file.
 
+## [1.0.2] - 2018-04-18
+
+### Fixed
+- load legacy javascript in front end mode only
+
 ## [1.0.1] - 2018-04-10
 
 ### Fixed
--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,3 +1,6 @@
 <?php
 
-$GLOBALS['TL_JAVASCRIPT']['bootstrap-hover-dropdown'] = 'bundles/heimrichhannotbootstraphoverdropdown/js/contao-bootstrap-hover-dropdown-bundle.min.js|static';
+if(\Contao\System::getContainer()->get('huh.utils.container')->isFrontend())
+{
+    $GLOBALS['TL_JAVASCRIPT']['bootstrap-hover-dropdown'] = 'bundles/heimrichhannotbootstraphoverdropdown/js/contao-bootstrap-hover-dropdown-bundle.min.js|static';
+}
